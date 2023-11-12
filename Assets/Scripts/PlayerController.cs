e27c07a52480d9da76aeadf3216d9e11441a2c87
Alex651907 <alex651907@gmail.com>
2023-11-12 18:02:43 -0500

    More platforming, lava death

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,11 +10,14 @@ public class PlayerController : MonoBehaviour
     public LayerMask whatIsTerrain;
     public Animator animator;
     private Rigidbody2D rb;
+    public Vector2 startingPos = Vector2.zero;
     private bool grounded;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        startingPos = transform.position;
+        
     }
 
     // Update is called once per frame
@@ -38,6 +41,11 @@ public class PlayerController : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
             animator.SetBool("grounded", false);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D co) {
+        if (co.tag == "lava"){
+            transform.position = startingPos;
         }
     }
 }
