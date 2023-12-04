4b73d075ffcc2ae7faf1ca7784da34d6ea82c51c
Tristan West <tlw66@drexel.edu>
2023-12-04 08:43:22 -0500

    Significant refactoring of the player controller script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -29,8 +29,6 @@ public class PlayerController : MonoBehaviour
     public GameObject[] powerUpAssets;
     public GameObject[] crabs;
     public GameObject powerUpItem;
-    private bool deathTimerStart = false;
-    private float deathAnimationTimer = 0f;
     private float dieRoationSpeed = 100f;
     private float dieScaleDownRate = .3f;
     public GameObject cameraObject;
@@ -38,6 +36,7 @@ public class PlayerController : MonoBehaviour
     public float maxBreath;
     private float breath;
     public AudioController audioController;
+    public bool controlsSuspended;
 
     // Start is called before the first frame update
     void Start()
@@ -50,9 +49,8 @@ public class PlayerController : MonoBehaviour
         jumpSpeed = defaultJumpSpeed;
         sprintSpeed = defaultSprintSpeed;
         foreach (GameObject asset in powerUpAssets)
-        {
             asset.SetActive(false);
-        }
+        controlsSuspended = false;
         sprinting = false;
         breath = maxBreath;
         playerLives.updateLives(Lives.GetLives());
@@ -61,11 +59,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!deathTimerStart)
-        {
-            grounded = Physics2D.OverlapBox(groundCheck.position, new Vector2(0.1f, 0.2f), 0f, whatIsTerrain);
-            wet = Physics2D.OverlapBox(groundCheck.position, new Vector2(0.1f, 0.2f), 0f, whatIsWater);
-            animator.SetBool("wet", wet);
+        grounded = Physics2D.OverlapBox(groundCheck.position, new Vector2(0.1f, 0.2f), 0f, whatIsTerrain);
+        wet = Physics2D.OverlapBox(groundCheck.position, new Vector2(0.1f, 0.2f), 0f, whatIsWater);
+        animator.SetBool("wet", wet);
 
         if (wet)
         {
@@ -74,9 +70,10 @@ public class PlayerController : MonoBehaviour
             if(breath < 0.001f)
             {
                 Lives.LoseLife();
-                transform.position = startingPos;
+                audioController.playDeathWater();
+                controlsSuspended = true;
+                StartCoroutine(playDeathAnimation(3.0f));
                 playerLives.updateLives(Lives.GetLives());
-                breath = maxBreath;
             }
             moveSpeed = defaultMoveSpeed - 2;
             sprintSpeed = defaultSprintSpeed - 2;
@@ -99,133 +96,60 @@ public class PlayerController : MonoBehaviour
         float breathNorm = Mathf.Clamp01(breath / maxBreath);
         breathMeter.GetComponent<Slider>().value = breathNorm;
 
+        if(!controlsSuspended)
             rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * (sprinting ? sprintSpeed : moveSpeed), rb.velocity.y);
-            animator.SetFloat("speed", rb.velocity.magnitude);
-            animator.SetBool("sprinting", sprinting && (grounded || wet));
+        animator.SetFloat("speed", rb.velocity.magnitude);
 
-            animator.SetBool("grounded", grounded);
-
-            if (Input.GetButtonDown("Jump") && (grounded || wet))
-            {
-                rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
-                animator.SetBool("grounded", false);
-                if (!wet)
-                {
-                    audioController.playJump();
-                }
-            }
-
-            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-                gameObject.GetComponent<SpriteRenderer>().flipX = true;
-
-                foreach (GameObject asset in powerUpAssets)
-                {
-                    bool facingLeft = asset.GetComponent<SpriteRenderer>().flipX;
-                    asset.GetComponent<SpriteRenderer>().flipX = true;
-
-                    if (asset.name == "scubaTank" && !facingLeft)
-                    {
-                        asset.transform.localPosition = new Vector3(-asset.transform.localPosition.x, asset.transform.localPosition.y, 0.0f);
-                    }
-                }
-            }
-            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                gameObject.GetComponent<SpriteRenderer>().flipX = false;
-
-                foreach (GameObject asset in powerUpAssets)
-                {
-                    bool facingRight = !asset.GetComponent<SpriteRenderer>().flipX;
-                    asset.GetComponent<SpriteRenderer>().flipX = false;
-
-                    if (asset.name == "scubaTank" && !facingRight)
-                    {
-                        asset.transform.localPosition = new Vector3(-asset.transform.localPosition.x, asset.transform.localPosition.y, 0.0f);
-                    }
-                }
-            }
-
-            if (Input.GetKeyDown(KeyCode.Q))
-            {
-                sprinting = true;
-
-            }
-            else if (Input.GetKeyUp(KeyCode.Q))
-            {
-                sprinting = false;
-            }
-        } 
-        else if (!(Vector3.Distance(transform.localScale, new Vector3(.1f, .1f, 1f)) < 0.01f))
+        animator.SetBool("sprinting", sprinting && (grounded || wet));
+        animator.SetBool("grounded", grounded);
+        
+        if (Input.GetButtonDown("Jump") && (grounded || wet))
         {
-            foreach (GameObject crab in crabs)
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+            animator.SetBool("grounded", false);
+            if (!wet)
             {
-                crab.GetComponent<CrabController>().ResetPosition();
-                crab.SetActive(false);
+                audioController.playJump();
             }
-
-            if (deathAnimationTimer == 0f)
-            {
-                if (wet)
-                {
-                    audioController.playDeathWater();
-                }
-                else
-                {
-                    audioController.playDeath();
-                }
-            }
-
-            transform.rotation *= Quaternion.Euler(0f, 0f, dieRoationSpeed * Time.deltaTime);
-            transform.localScale = transform.localScale - new Vector3(dieScaleDownRate, dieScaleDownRate, 0) * Time.deltaTime;
-
-            deathAnimationTimer += 1f;
         }
-        else
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            if(crabs.Length > 0)
-            {
-                foreach (GameObject crab in crabs)
-                {
-                    crab.SetActive(true);
-                }
-            }
-
-            if(powerUpAssets.Length > 0){
-                powerUpItem.SetActive(true);
-
-                foreach (GameObject asset in powerUpAssets)
-                {
-                    asset.SetActive(false);
-                }
-            }
-
-            transform.position = startingPos;
-            transform.localScale = startingScale;
-            transform.rotation = startingRotation;
-
-            deathAnimationTimer = 0f;
-            deathTimerStart = false;
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            foreach (GameObject asset in powerUpAssets)
+                asset.GetComponent<SpriteRenderer>().flipX = true;
         }
-
-       
-    }
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            foreach (GameObject asset in powerUpAssets)
+                asset.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Q) && grounded)
+        {
+            sprinting = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.Q))
+        {
+            sprinting = false;
+        }
+    } 
     void OnTriggerEnter2D(Collider2D co)
     {
         if (co.tag == "lava" || co.tag == "enemy"){
             Lives.LoseLife();
             playerLives.updateLives(Lives.GetLives());
-            deathTimerStart = true;
+            audioController.playDeath();
+            controlsSuspended = true;
+            StartCoroutine(playDeathAnimation(3.0f));
         }
         if (Lives.GetLives() <= 0) {
-            audioController.playGameOver();
             SceneManager.LoadScene(gameOverScene);
         }
 
         if (co.tag == "power")
         {
             audioController.playPowerUpCollect();
-            co.gameObject.SetActive(false);
+            powerUpItem.SetActive(false);
             foreach (GameObject asset in powerUpAssets)
             {
                 asset.SetActive(true);
@@ -236,5 +160,49 @@ public class PlayerController : MonoBehaviour
         {
             co.gameObject.GetComponent<AudioSource>().Play();
         }
+    }
+
+    IEnumerator playDeathAnimation(float duration)
+    {
+        if(crabs.Length > 0)
+        {
+            foreach (GameObject crab in crabs)
+            {
+                crab.GetComponent<CrabController>().ResetPosition();
+                crab.SetActive(false);
+            }
+        }
+        float timer = Time.time;
+        float iniTime = timer;
+        rb.constraints = RigidbodyConstraints2D.FreezePosition;
+        while(timer < iniTime + duration){
+            transform.rotation *= Quaternion.Euler(0f, 0f, dieRoationSpeed * Time.deltaTime);
+            transform.localScale = transform.localScale - new Vector3(dieScaleDownRate, dieScaleDownRate, 0) * Time.deltaTime;
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        respawn();
+    }
+
+    void respawn()
+    {
+        transform.position = startingPos;
+        transform.localScale = startingScale;
+        transform.rotation = startingRotation;
+        breath = maxBreath;
+        if(crabs.Length > 0)
+        {
+            foreach (GameObject crab in crabs) 
+                crab.SetActive(true);
+        }
+        if(powerUpAssets.Length > 0){
+            powerUpItem.SetActive(true);
+            foreach (GameObject asset in powerUpAssets)
+            {
+                asset.SetActive(false);
+            }
+        }
+        controlsSuspended = false;
     }
 }
